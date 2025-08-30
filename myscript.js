9edbf78d4f16c01be8fcd43074048e31c4b2b45b
lilyjge <lilyjge@gmail.com>
2025-08-29 23:29:28 -0400

    revamp

--- myscript.js
+++ myscript.js
@@ -38,3 +38,35 @@ for(i = 0; i < navlinks.length; i++){
     content.style.overflowY = "visible";
   });
 }
+
+// Photo Modal Functionality
+function openModal(element) {
+  var modal = document.getElementById("photoModal");
+  var modalImg = document.getElementById("modalImg");
+  var img = element.querySelector('img');
+  
+  modal.style.display = "flex";
+  modalImg.src = img.src;
+  modalImg.alt = img.alt;
+}
+
+function closeModal() {
+  var modal = document.getElementById("photoModal");
+  modal.style.display = "none";
+}
+
+// Close modal when clicking outside the image
+window.onclick = function(event) {
+  var modal = document.getElementById("photoModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Close modal with Escape key
+document.addEventListener('keydown', function(event) {
+  if (event.key === "Escape") {
+    var modal = document.getElementById("photoModal");
+    modal.style.display = "none";
+  }
+});
